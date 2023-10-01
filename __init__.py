e176c3db05ba4b21414aadc3087f47800a1d246d
Hamlet <hamlets849@gmail.com>
2023-10-01 23:46:33 +0400

    .

--- __init__.py
+++
@@ -1,6 +0,0 @@
-import main
-import utils
-import variables
-import parts
-
-__all__ = ['main', 'utils', 'variables', 'parts']
--- parts/__init__.py
+++
@@ -1,5 +0,0 @@
-import main
-import variables
-import utils
-
-__all__ = ['main', 'utils', 'variables']
