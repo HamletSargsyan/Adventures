6a4543a0ca6da7f3489c6bda9e055f1ed44ff2fe
Hamlet <hamlets849@gmail.com>
2023-10-05 23:14:06 +0400

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def start_menu():
                           ("Играть", "1"),
                           ("Обновления", "2"),
                           ("Выгрузить", "3"),
-                          ("Справка", "4")
+                        #   ("Справка", "4")
                       ],
                       ),
     ]
@@ -39,9 +39,9 @@ def start_menu():
     elif choice == '3':
         load_game()
         profile()
-    # elif choice == '4':
-        # from parts.help import help
-        # help()
+    elif choice == '4':
+        from parts.help import help
+        help()
 
 if __name__ == '__main__':
     start_menu()
